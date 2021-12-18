eef527746cdc00b8a87e5bcf7955fdfc70f98c1d
SBilousko <s.bilousko@ukr.net>
2021-12-18 02:10:54 +0200

    Components Composition

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { render } from "react-dom";
+import React from "react";
 
 const Counter = (props) => {
   const { value } = props;
@@ -7,7 +6,6 @@ const Counter = (props) => {
   const formatValue = () => {
     return value === 0 ? "Empty" : value;
   };
-  const imageURL = "https://picsum.photos/200";
 
   const getBadgeClasses = () => {
     let classes = "badge m-2 ";
@@ -19,10 +17,16 @@ const Counter = (props) => {
     <div>
       <span>{props.name}</span>
       <span className={getBadgeClasses()}>{formatValue()}</span>
-      <button className="btn btn-primary btn-sm m-2" onClick={() => props.onIncrement(props.id)}>
+      <button
+        className="btn btn-primary btn-sm m-2"
+        onClick={() => props.onIncrement(props.id)}
+      >
         +
       </button>
-      <button className="btn btn-primary btn-sm m-2" onClick={() => props.onDecrement(props.id)}>
+      <button
+        className="btn btn-primary btn-sm m-2"
+        onClick={() => props.onDecrement(props.id)}
+      >
         -
       </button>
       <button
--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -18,16 +18,34 @@ const CountersList = () => {
     setCounters(initialState);
   };
   const handleIncrement = (id) => {
-    initialState[id].value += 1
+    const newCounter = counters.map((counter) => {
+      if (counter.id === id) {
+        counter.value += 1;
+      }
+      return counter;
+    });
+    setCounters(newCounter);
   };
   const handleDecrement = (id) => {
-    setCounters(initialState[id].value);
+    const newCounter = counters.map((counter) => {
+      if (counter.id === id) {
+        counter.value -= 1;
+      }
+      return counter;
+    });
+    setCounters(newCounter);
   };
 
   return (
     <>
       {counters.map((count) => (
-        <Counter {...count} key={count.id} onDelete={handleDelete} onIncrement={handleIncrement} onDecrement={handleDecrement} />
+        <Counter
+          {...count}
+          key={count.id}
+          onDelete={handleDelete}
+          onIncrement={handleIncrement}
+          onDecrement={handleDecrement}
+        />
       ))}
       <button className="btn btn-primary btn-sm m-2" onClick={handleReset}>
         Сброс
